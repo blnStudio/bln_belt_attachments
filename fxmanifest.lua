99a41f56b9b4fccb42599b726a496de5aa7ff2d3
munafio <munafaqeelmahdi@gmail.com>
2025-01-07 18:15:00 +0300

    added: version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'BLN Studio <bln.tebex.io>'
 description 'Realistically display your weapons and items on your character\'s belt when equipped from inventory but not in use! Works with lanterns, lassos, machetes, and more.'
-version '1.2.0'
+version '1.2.1'
 
 client_scripts {
     'c/*.lua'
